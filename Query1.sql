062d94f068ce79c91250fdd0914ec99a4e9e973a
alklas <145354121+alklas@users.noreply.github.com>
2024-05-04 14:30:30 +0700

    Add files via upload
    Простые запросы к таблицам. Запросы с условиями.

---
+++ Query1.sql
@@ -0,0 +1,36 @@
+SELECT 
+    `discipl`.`pnamedisc` as 'Полное наименование дисциплины'
+FROM
+    `discipl`;
+SELECT DISTINCT
+    `groups`.`napr` as 'Направления подготовки'
+FROM
+    `groups`;
+SELECT 
+    *
+FROM
+    `discipl`
+WHERE
+    `discipl`.`knamedisc` = 'ЭП';    
+SELECT 
+    *
+FROM
+    `discipl`
+WHERE
+    `iddiscipl` <> 5;
+SELECT 
+    `name`, `dob`
+FROM
+    `students`
+WHERE
+    `idgroups` <> 2 AND dob > '2000-11-15';
+SELECT 
+    `name`, `dob`
+FROM
+    `students`
+WHERE
+	NOT (`idgroups` <> 2 OR dob < '2000-11-15');
+/*
+Выводим тех студентов, у которых дата рождения позже 15.11.2000 и которые 
+при этом учатся в группе с idgroup = 2.
+*/    
