f96f3d54bf6ed56fef882b213ea494a895e0442c
alklas <145354121+alklas@users.noreply.github.com>
2024-05-24 20:02:25 +0700

    Add files via upload
    Добавили запросы на сортировку значений

--- Query3.sql
+++ Query3.sql
@@ -39,4 +39,88 @@ FROM
     students
 WHERE
     dob < date('1999-12-31') + (DATE('2000-12-01') - DATE('2000-01-01'));
+/* Проверка условия в предложии SELECT */    
+SELECT 
+    case `students`.`docum`
+    when '1'
+    then `students`.`docum`
+    else 'Нет документов'
+    end as 'Наличие документов', `students`.`name`
+FROM
+    students
+    where `students`.`docum` <> 0 or `students`.`docum` is NULL;
+
+SELECT 
+    case `students`.`docum`
+    when '1'
+    then `students`.`docum`
+    when '0'
+    then '0 документов'
+    else 'Нет документов'
+    end as 'Наличие документов', `students`.`name`
+FROM
+    students
+    where 1;
     
+/* Изменения в шифрах направлений (например, на устаревшие) */
+    SELECT 
+    case shifr_napr
+    when '09.03.02'
+    then '11.11.11'
+    when '44.03.05'
+    then '22.22.22'
+    else shifr_napr
+    end as 'Устаревшие шифры направлений', `groups`.`napr`
+FROM
+    `groups`
+    where 1;
+/* Упрощённая форма работы с совпадающими значениями - заменяет второй параметр функции nullif() на значение NULL, если он совпадает с содержимым ячейки */    
+   SELECT 
+     nullif(shifr_napr, '09.03.02'), napr
+FROM
+    `groups`
+    where 1; 
+/* Сортировка результатов отбора */
+SELECT 
+    *
+FROM
+    students
+ORDER BY dob DESC , `name` DESC;
+
+SELECT 
+    *
+FROM
+    students
+ORDER BY `name` DESC;
+
+SELECT 
+    *
+FROM
+    students
+ORDER BY idgroups DESC;
+/* Самостоятельный запрос на сортировку значений */
+
+/* Упорядочивание агрегатов */
+SELECT 
+    `hours`, COUNT(knamedisc)
+FROM
+    discipl
+GROUP BY hours
+ORDER BY COUNT(knamedisc);
+
+SELECT 
+    `hours`, COUNT(knamedisc)
+FROM
+    discipl
+GROUP BY hours
+ORDER BY 2 desc;
+
+/* Самостоятельный запрос на сортировку максимальных дат рождения по академической группе  */
+SELECT 
+    idgroups, MAX(dob)
+FROM
+    students
+WHERE
+    NOT idgroups IS NULL
+GROUP BY idgroups
+ORDER BY 2;
