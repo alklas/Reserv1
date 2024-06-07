5d1b9059851a0b54646c2fb34f2af116e230d5a0
alklas <145354121+alklas@users.noreply.github.com>
2024-06-07 18:13:40 +0700

    Add files via upload
    Добавили более сложные примеры соединения таблиц

--- Query4.sql
+++ Query4.sql
@@ -42,7 +42,7 @@ FROM
     students
         CROSS JOIN
     `groups` USING (idgroups)
-ORDER BY `name`;
+ORDER BY `name` ;
 
 /* Ещё одна форма соединения таблиц, добавили условие отбора по направлению */
 SELECT 
@@ -53,3 +53,55 @@ FROM
     `groups` ON (students.idgroups = `groups`.idgroups)
 WHERE
     napr LIKE 'И%';
+    
+/* Использование составных условий при соединении таблиц */
+SELECT 
+    `students`.`name`, `groups`.`napr`, `groups`.`prof`
+FROM
+    students,
+    `groups`
+WHERE
+    shifr_napr IN ('09.03.02' , '44.03.02')
+        AND students.idgroups = `groups`.idgroups;
+        
+/* Использование операторов сравнения при соединении */
+SELECT 
+    `students`.`name`, `groups`.`napr`, `groups`.`prof`
+FROM
+    students,
+    `groups`
+WHERE
+    shifr_napr > '09.03.02'
+        AND students.idgroups < `groups`.idgroups;
+        
+/* Соединение таблицы с собой */
+SELECT 
+    `first`.`name`, `second`.`name`
+FROM
+    students `first`,
+    students `second`
+WHERE
+    `first`.idgroups = `second`.idgroups
+        AND `first`.`name` > `second`.`name`;
+        
+/* Поиск образцов */
+SELECT 
+    a.`name` as 'Группа 3.054.2.19', b.`name` as 'Группа 3.114.2.21', c.`name` as 'Группа 3.008.1.20'
+FROM
+    students a,
+    students b,
+    students c
+WHERE
+    a.idgroups = 2
+        AND b.idgroups = 3
+        AND c.idgroups = 5;
+
+/* Соединение таблиц, не входящих в результат запроса */
+SELECT 
+    `students`.`name`, `students`.`dob`
+FROM
+    students,
+    `groups`
+WHERE
+    shifr_napr = '09.03.02'
+        AND students.idgroups = `groups`.idgroups;
